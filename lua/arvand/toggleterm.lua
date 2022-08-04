6ad992b965c9fc30b8bb17bc8422ff5dfc8dd053
vandool <arvandkaveh@gmail.com>
2022-08-04 07:43:03 +0200

    project added

--- lua/arvand/toggleterm.lua
+++ lua/arvand/toggleterm.lua
@@ -8,7 +8,7 @@ toggleterm.setup({
 	open_mapping = [[<c-.>]],
 	hide_numbers = true,
 	shade_terminals = true,
-	shading_factor = 2,
+	shading_factor = 1, -- the degree by which to darken to terminal colour, default: 1 for dark backgrounds, 3 for light
 	start_in_insert = true,
 	insert_mappings = true,
 	persist_size = true,
